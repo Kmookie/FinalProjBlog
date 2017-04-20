76197d6301888f9a22fb29af80279f1a5390de42
MorganD <morgan_durocher@hotmail.com>
2017-04-20 19:54:22 +0000

    Got my models, Views, Apps, code all written. Not running yet

---
+++ blog_project/models.py
@@ -0,0 +1,13 @@
+from __future__ import unicode_literals
+from django.utils.encoding import python_2_unicode_compatible
+
+from django.db import models
+
+
+
+@python_2_unicode_compatible  # only if you need to support Python 2
+class Post(models.Model):
+    
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
---
+++ blog_project/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import get_object_or_404, render
+from django.http import Http404, HttpResponseRedirect, HttpResponse
+from .models import Post
+from django.template import loader
+from django.core.urlresolvers import reverse
+from django.views import generic
+
+
+class IndexView(generic.ListView):
+    template_name = 'index.html'
+    context_object_name = 'blog_posts'
+
+    def get_queryset(self):
+        return Post.objects.order_by('timestamp')
+
+def edit_post(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    print post
+    
+
+def add_post(request, post_id):
+    post = Post()
+    post.title = request.POST['title']
+    post.body = request.POST['body']
+    post.save()
+    
+
+# def vote(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     try:
+#         selected_choice = question.choice_set.get(pk=request.POST['choice'])
+#     except (KeyError, Choice.DoesNotExist):
+#         # Redisplay the question voting form.
+#         return render(request, 'polls/detail.html', {
+#             'question': question,
+#             'error_message': "You didn't select a choice.",
+#         })
+#     else:
+#         selected_choice.votes += 1
+#         selected_choice.save()
+#         # Always return an HttpResponseRedirect after successfully dealing
+#         # with POST data. This prevents data from being posted twice if a
+#         # user hits the Back button.
+#         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
